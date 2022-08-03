5ed6d75ee1380a441a1aba721ec946d5b7f85416
Jaden Wijata <46347858+xenonminer@users.noreply.github.com>
2022-08-02 17:14:46 -0700

    Update lib.rs

--- rustzipworker/src/lib.rs
+++ rustzipworker/src/lib.rs
@@ -1,4 +1,3 @@
-use serde_json::json;
 use worker::*;
 
 mod utils;
@@ -29,7 +28,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     // functionality and a `RouteContext` which you can use to  and get route parameters and
     // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
 
-    const htmlcode: &str = r#"
+    const FILEPAGE: &str = r#"
     <!DOCTYPE HTML>
     <html>
         <head>
@@ -45,38 +44,81 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 cursor: text;
             }
             </style>
-            <h1 style="padding-top: 5%;">Auto File Decompressor</h1>
-            <h2>Browse for a file you would like to have decompressed!</h2>
+            <h1 style="padding-top: 5%;">Faas</h1>
+            <h2>see the filetype of your file</h2>
             <form method="post" action="/" enctype="multipart/form-data" style="padding-top: 2%;">
                 <label for="myfile">Select a file:</label>
-                <input type="file" id="myfile" name="myfile" accept="application/gzip,application/x-bzip2,application/x-lzip,application/x-lzma,application/x-7z-compressed,application/x-rar-compressed,application/x-gtar,application/zip"> 
+                <input type="file" id="myfile" name="myfile"> 
                 <input type="submit" class="submit" value="Upload">
             </form>
             <p style="padding-top: 20px; color: red;"></p>
+            <p>Other pages:</p>
+            <p><a href="/html">html reader</a></p> 
         </body>
     </html>
     "#; // use const instead of let to make variable global
 
+    const HTMLPAGE: &str = r#"
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <title>HTML Displayer</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        </head>
+        <body style="text-align: center; background-color:powderblue;">
+            <style>
+            #fill {
+                border-radius: 8px;
+                padding: 10px;
+                display: inline-block;
+                cursor: text;
+            }
+            </style>
+            <h1 style="padding-top: 5%;">Haas</h1>
+            <h2>render your html text</h2>
+            <form method="post" action="/html" enctype="multipart/form-data" style="padding-top: 2%;">
+                <label for="htmlcode">html text:</label><br>
+                <textarea id="htmlcode" name="htmlcode" rows="10" cols="100">Enter your html here</textarea><br><br>
+                <input type="submit" value="Submit">
+            </form>
+            <p style="padding-top: 20px; color: red;"></p>
+            <p>Other pages:</p>
+            <p><a href="/">filetype display</a></p> 
+        </body>
+    </html>
+    "#;
+
     router
-        .get("/", |_, _| Response::from_html(htmlcode))
+        .get("/", |_, _| Response::from_html(FILEPAGE))
         .post_async("/", |mut req, _ctx| async move {
             let form = req.form_data().await?;
-            dbg!(&form);
             if let Some(entry) = form.get("myfile") {
                 match entry {
                     FormEntry::File(file) => {
                         let filetype = file.type_();
-                        return Response::ok(filetype)
+                        return Response::ok(filetype);
                     }
-                    FormEntry::Field(_) => return Response::error("Bad Request a", 400),
+                    FormEntry::Field(_) => return Response::error("Bad Request", 400),
                 }
 
-                //if !(filetype.iter().any(|i| i=="zip") && filetype.iter().any(|i| i=="tar")) {
-                //    Response::error("Bad Request", 400)
-                //}
-
             }
-            Response::error("Bad Request b", 400)
+            Response::error("Bad Request", 400)
+        })
+
+        .get("/html", |_, _| Response::from_html(HTMLPAGE))
+        .post_async("/html", |mut req, _ctx| async move {
+            let form = req.form_data().await?;
+            if let Some(name) = form.get("htmlcode") {
+                match name {
+                    FormEntry::Field(htmltext) => {
+                        let htmlstuff = &htmltext;
+			            return Response::from_html(htmlstuff)
+                    }
+                    FormEntry::File(_) => return Response::error("Bad Request", 400),
+ 
+                }
+            }
+            Response::error("Bad Request", 400)
         })
         .run(req, env).await
 }
